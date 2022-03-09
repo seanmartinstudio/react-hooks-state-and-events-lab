72daf581b852f619e2d113b1d4847193aea21c2b
Sean Martin <sean@seanmartinstudio.com>
2022-03-09 13:49:40 -0800

    Done.

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -4,12 +4,16 @@ import Item from "./Item";
 function ShoppingList({ items }) {
 
 
-const [selectedCategory, updateState] = useState()
+const [selectedCategory, updateState] = useState("All")
+
+function handleList(event) {
+  updateState()
+}
 
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleList}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
